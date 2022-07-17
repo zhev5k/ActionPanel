5e0eb4d5857108eae858dd48a51b82c24956cf9a
zhevonez <96353840+zhevonez@users.noreply.github.com>
2022-07-17 21:16:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,136 @@
+import time
+import sys
+import os
+from rich import print
+
+def typewriter(mes1):
+	for char in mes1:
+		sys.stdout.write(char)
+		sys.stdout.flush()
+		time.sleep(0.02)
+
+mes1 = ''' ______   _ _______     _____  _   _ _____ _____
+|__  / | | | ____\ \   / / _ \| \ | | ____|__  /
+  / /| |_| |  _|  \ \ / / | | |  \| |  _|   / /
+ / /_|  _  | |___  \ V /| |_| | |\  | |___ / /_
+/____|_| |_|_____|  \_/  \___/|_| \_|_____/____|
+'''
+os.system('cls')
+typewriter(mes1)
+time.sleep(3)
+os.system('cls')
+
+name = input("Введите ваше имя: ")
+name2 = input("Ваше имя обидчика: ")
+
+os.system('cls')
+
+def mainmenu():
+	print('''
+[1] Ударить
+[2] Обосрали
+[3] Убили
+[4] Выебать
+[5] Унизить
+[6] Обоссать
+[7] Обкончать
+[8] Сватнуть''')
+
+mainmenu()
+
+choice = input("Выбери действие: ")
+if not choice.isdigit():
+	print("Ты аутист? Число выбери")
+	exit(0)
+choice = int(choice)
+
+
+
+if choice == 1:
+	print(name2, "ударил", name )
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+
+if choice == 2:
+	print(name2, "обосрал", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+if choice == 3:
+	print(name2, "убил", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+if choice == 4:
+	print(name2, "выебал", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+if choice == 5:
+	print(name2, "унизил", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+if choice == 6:
+	print(name2, "обоссал", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+if choice == 7:
+	print(name2, "обкончал", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
+
+if choice == 8:
+	print(name2, "кончил на", name)
+	time.sleep(5)
+	os.system('cls')
+	mainmenu()
+	choice = input("Выбери действие: ")
+	if not choice.isdigit():
+		print("Ты аутист? Число выбери")
+		exit(0)
+	choice = int(choice)
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Programm by zhevonez
+For installation:
+pip install -r requirements.txt
+after:
+git clone https://github.com/zhevonez/ActionPanel
+cd ActionPanel
+python3 main.py
